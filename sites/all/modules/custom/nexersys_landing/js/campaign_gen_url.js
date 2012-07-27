07c60cbe0f55e7be9ad602ad858fef2c7e9adf70
Jordan Austin <jordotech@Jordan-Austins-MacBook-Pro.local>
2012-07-27 11:42:20 -0500

    url builder working

--- sites/all/modules/custom/nexersys_landing/js/campaign_gen_url.js
+++ sites/all/modules/custom/nexersys_landing/js/campaign_gen_url.js
@@ -2,11 +2,27 @@ var $ = jQuery.noConflict();
 $(document).ready(function() {
     
     $('.gen_url_link').click(function(){
-        var genUrl = '';
-        $('#campaign-node-form .form-text input').each(function(){
-            console.log($(this).val());
+        var genUrl = 'http://www.nexersystv.com?';
+        var urlParams = '';
+        //http://test.com/?utm_source=citytheft&utm_medium=banner&utm_term=bike&utm_content=10%25%2Bdiscount&utm_campaign=roberts%2Bcampaign
+        $('#campaign-node-form .urlVar').each(function(){
+            if($(this).hasClass('utm_src') && $(this).val() != ''){
+                urlParams += '&utm_src=' + encodeURIComponent($(this).val());
+            }
+            if($(this).hasClass('utm_content') && $(this).val() != ''){
+                urlParams += '&utm_content=' + encodeURIComponent($(this).val());
+            }
+            if($(this).hasClass('utm_media') && $(this).val() != ''){
+                urlParams += '&utm_media=' + encodeURIComponent($(this).val());
+            }
+            if($(this).hasClass('utm_term') && $(this).val() != ''){
+                
+                urlParams += '&utm_term=' + encodeURIComponent($(this).val());
+            }
         });
-    });
-    //$('.field-name-field-gen-url input').val(genUrl);
-    
+                    urlParams = urlParams.substr(1);
+            genUrl = genUrl + urlParams;
+        $('#campaign-node-form .genUrl').val(genUrl);
+        console.log(genUrl);
+    });    
 });
