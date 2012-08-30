09719c710830efbaf9447e07f4a19afc1f4231e3
Jordan Austin <jordotech@Jordan-Austins-MacBook-Pro.local>
2012-08-30 16:41:10 -0500

    forwarding working

--- sites/all/modules/custom/camp_landing/js/campaign_landing.js
+++ sites/all/modules/custom/camp_landing/js/campaign_landing.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-var cookie = $.cookie('nexersystv_campaign');
-if(cookie != ''){
-    cookie =     $.parseJSON(cookie);
-    phone = cookie['campaign_phone'];
+    var cookie = $.cookie('nexersystv_campaign');
+    if(cookie != ''){
+        cookie =     $.parseJSON(cookie);
+        phone = cookie['campaign_phone'];
+    }else{
+        
+    }
     $('.camp_landing_phone').html(phone);
-
-}
-    
-
 });
