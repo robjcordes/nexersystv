331ec18e9933d11a512575f56f6728d0ec6b606c
Jordan Austin <jordotech@Jordan-Austins-MacBook-Pro.local>
2012-09-04 14:22:40 -0500

    cleaning up

--- sites/all/modules/custom/camp_landing/js/campaign_landing.js
+++ sites/all/modules/custom/camp_landing/js/campaign_landing.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
         phone = cookie['campaign_phone'];
 
     $('.camp_landing_phone').html(phone);
+    $('.panel-box dd').html(phone);
     }
     $('.view-mode-full').append('<a href="#" onclick="camp_landing_delete_cookie();">Delete campaign cookie</a>');
 
---
+++ sites/all/themes/nexnewwp/templates/section--footer.tpl.php
@@ -0,0 +1,84 @@
+<footer<?php print $attributes; ?>>
+    <div class="footer-holder">
+				<div class="footer-frame">
+					<div class="container_12">
+						<!-- footer container -->
+						<div class="footer-container">
+							 
+                                                            <div class="links-box">
+                                                                <div class="col"><h4><a href="#">Products</a></h4><div class="menu-products-container"><ul class="" id="menu-products"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-266" id="menu-item-266"><a href="http://nexersys.com/home-fitness-3/">Home Model (NXS-H)</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-69" id="menu-item-69"><a href="http://nexersys.com/pro-fitness/">Pro Model (NXS-P)</a></li>
+<li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-598" id="menu-item-598"><a href="/commercial-fitness/">Commercial (NXS-C)</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-71" id="menu-item-71"><a href="http://nexersys.com/videos/">Videos</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-73" id="menu-item-73"><a href="http://nexersys.com/accessories/">Accessories</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-861" id="menu-item-861"><a href="http://nexersys.com/buy-now/">Buy Now</a></li>
+</ul></div></div><div class="col"><h4><a href="#">Information</a></h4><div class="menu-information-container"><ul class="" id="menu-information"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-75" id="menu-item-75"><a href="http://nexersys.com/faq/">FAQ</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-76" id="menu-item-76"><a href="http://nexersys.com/warranty/">Warranty</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-862" id="menu-item-862"><a href="http://nexersys.com/buy-now/">Our Products</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78" id="menu-item-78"><a href="http://nexersys.com/compare-nexersys/">Compare Nexersys</a></li>
+</ul></div></div><div class="col"><h4><a href="#">Company</a></h4><div class="menu-company-container"><ul class="" id="menu-company"><li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-79" id="menu-item-79"><a href="http://nexersys.com/fitness-equipment-company/">About Us</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-80" id="menu-item-80"><a href="http://nexersys.com/contact-us/">Contact Us</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-490" id="menu-item-490"><a href="http://nexersys.com/careers/">Careers</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-81" id="menu-item-81"><a href="http://nexersys.com/terms-of-use/">Terms of Use</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-82" id="menu-item-82"><a href="http://nexersys.com/privacy-policy/">Privacy Policy</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-83" id="menu-item-83"><a href="http://nexersys.com/financing/">Financing</a></li>
+</ul></div></div><div class="col"><h4><a href="#">Media</a></h4><div class="menu-media-container"><ul class="" id="menu-media"><li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-180" id="menu-item-180"><a href="http://exercise.nexersys.com/">Our Blog</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-85" id="menu-item-85"><a href="http://nexersys.com/testimonials/">Testimonials</a></li>
+<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-86" id="menu-item-86"><a href="http://nexersys.com/press-media/">Press &amp; Media</a></li>
+</ul></div></div>	
+                                                            </div>
+                                                        							<!-- item column -->
+							<div class="item-col">
+					<ul class="social-networks">
+									<li><a href="http://www.facebook.com/pages/Nexersys/147238475330559">facebook</a></li>
+									<li><a class="twitter" href="http://twitter.com/#!/NEXERSYS/">twitter</a></li>
+									<li><a class="you-tube" href="http://www.youtube.com/NEXERSYS">you-tube</a></li>
+									
+								</ul>			
+							</div>
+						</div>
+						 
+                                                    <div class="partner-box">
+                                                            <div class="holder">
+                                                                <div class="frame">			<img width="151" height="40" alt="" src="http://nexersys.com/wp-content/themes/nexersys/images/partner1.png">
+                                                                            <ul>
+                                                                                    <li><img width="46" height="40" alt=" " src="/sites/all/themes/nexnewwp/images/partner2.png"></li>
+                                                                                    <li><img width="53" height="40" alt=" " src="/sites/all/themes/nexnewwp/images/partner3.png"></li>
+                                                                                    <li><img width="50" height="40" alt=" " src="/sites/all/themes/nexnewwp/images/partner4.png"></li>
+                                                                                    <li><img width="46" height="40" alt=" " src="/sites/all/themes/nexnewwp/images/partner5.png"></li>
+                                                                                                                                                       
+<li>
+<!-- START VOL SEAL CODE -->
+
+            <a href="javascript:void(0);" onclick="window.open('https://www.volusion.com/ssl.asp?url=store.nexersys.com', 'VolusionSSL', 'top=10,left=10,menubar=0,resizable=0,scrollbars=0,width=467,height=467')"> <img width="144px" height="55px" border="0" id="volusion_ssl_seal" src="http://nexersys.com/wp-content/themes/nexersys/images/footer-seals-volusion.png"></a>
+
+            <!-- END VOL SEAL CODE -->
+</li>
+
+<li>
+            <!-- START BBB CODE -->
+
+             <a href="http://www.bbb.org/central-texas/business-reviews/exercise-equipment/nexersys-in-austin-tx-90101698#sealclick" title="Click for the Business Review of Nexersys, an Exercise Equipment in Austin TX"><img width="96" height="50" src="http://seal-austin.bbb.org/seals/blue-seal-96-50-nexersys-90101698.png" style="border: 0;" alt="Click for the BBB Business Review of this Exercise Equipment in Austin TX"></a>
+
+            <!-- END BBB CODE -->
+</li>
+
+<li>
+             <!-- START SCANALERT CODE -->
+
+             <a href="https://www.mcafeesecure.com/RatingVerify?ref=www.nexersys.com" target="_blank"><img width="115" height="32" border="0" style="padding-bottom: 13px; padding-left: 5px; height: 35px;" oncontextmenu="alert('Copying Prohibited by Law - McAfee SECURE is a Trademark of McAfee, Inc.'); return false;" alt="McAfee SECURE sites help keep you safe from identity theft, credit card fraud, spyware, spam, viruses and online scams" src="//images.scanalert.com/meter/www.nexersys.com/22.gif"></a>
+
+             <!-- END SCANALERT CODE -->
+</li>
+
+ </ul>
+		</div>	
+                                                            </div>
+                                                    </div>
+                                                					</div>
+				</div>
+			</div>
+  <?php print $content; ?>
+    
+    
+</footer>
