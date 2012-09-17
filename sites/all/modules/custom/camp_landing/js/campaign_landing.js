9972f68b663858d5068e4b51020aea49289129ca
Jordan Austin <jordotech@Jordan-Austins-MacBook-Pro.local>
2012-09-17 13:37:36 -0500

    uid tracking, livequery, 3rd party cookie

--- sites/all/modules/custom/camp_landing/js/campaign_landing.js
+++ sites/all/modules/custom/camp_landing/js/campaign_landing.js
@@ -20,16 +20,30 @@ $(document).ready(function() {
         var footerLink = $(this).children().attr('href');
         adjustedFooterLink = footerLink + '?' + params;
         $(this).children().attr('href', adjustedFooterLink);
-        //console.log(footerLink);
-        //console.log(params);
+
     });
 
 
     
 });
+   
+   //show/hide the company name field on generic versions of the form based on the model selected
+   $('.company_name.togglable').hide();
+   $('.company_control').livequery('change', function(event) { 
+                             var modelTypeSelected = $('.company_control').val();
+                             if(modelTypeSelected == 'Commercial'){
+                                 $('.company_name').show();
+                                 console.log('selected com');
+                             }else{
+                                 
+                                 $('.company_name').hide();
+                             }
+                            return false; 
+                         }); 
     
     
 });
     function camp_landing_delete_cookie(){
         $.removeCookie('nexersystv_campaign');
+        location.reload();
     }
